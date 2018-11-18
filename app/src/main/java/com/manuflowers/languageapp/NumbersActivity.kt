e9683c112b9104213f941c93c847fe48eaec6947
Manuela Flores <fmanuela499@gmail.com>
2018-11-18 16:19:19 -0500

    add listview to numbersActivity

--- app/src/main/java/com/manuflowers/languageapp/NumbersActivity.kt
+++ app/src/main/java/com/manuflowers/languageapp/NumbersActivity.kt
@@ -3,6 +3,10 @@ package com.manuflowers.languageapp
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.TextView
 
 class NumbersActivity : AppCompatActivity() {
 
@@ -21,7 +25,11 @@ class NumbersActivity : AppCompatActivity() {
             "seven",
             "eight",
             "nine",
-            "ten")
-        Log.v("Numbers Activity", "${numbers[0]}")
+            "ten"
+            )
+
+        var itemsAdapter: ArrayAdapter<String> = ArrayAdapter(this, android.R.layout.simple_list_item_1, numbers)
+        var listView: ListView = findViewById(R.id.lvNumbersContainer)
+        listView.adapter = itemsAdapter
     }
 }
